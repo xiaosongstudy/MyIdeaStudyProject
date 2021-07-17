96d126be8666ba7d32c14796e573ccdca963b8a5
xiaosongstudy <2453332538@qq.com>
2021-07-17 20:55:34 +0800

    第二次提交TestDemo01.java代码 添加了两个方法

--- git-test/src/life/hopeurl/git/TestDemo01.java
+++ git-test/src/life/hopeurl/git/TestDemo01.java
@@ -7,5 +7,16 @@ package life.hopeurl.git;
 public class TestDemo01 {
     public static void main(String[] args) {
         System.out.println("我是谁？");
+        System.out.println("我是谁");
+    }
+
+
+    public static void printInfo(){
+        System.out.println("我是一个小鹏ing有手动阀手动阀是否士大夫十三点发f");
+    }
+
+
+    public boolean isNull(){
+        return false;
     }
 }
