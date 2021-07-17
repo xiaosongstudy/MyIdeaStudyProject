11fddaf4003579fa1aedbe30182e3d4f92ee749c
xiaosongstudy <2453332538@qq.com>
2021-07-17 20:53:13 +0800

    第一次提交TestDemo01.java代码 只有输出语句 System.out.println("我是谁？");

---
+++ git-test/src/life/hopeurl/git/TestDemo01.java
@@ -0,0 +1,11 @@
+package life.hopeurl.git;
+
+/**
+ * @author 宋世平
+ * @time 2021/7/17
+ */
+public class TestDemo01 {
+    public static void main(String[] args) {
+        System.out.println("我是谁？");
+    }
+}
